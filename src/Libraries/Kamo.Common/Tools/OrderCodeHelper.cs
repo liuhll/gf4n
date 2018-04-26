c5302f86c87a61097d936f18994047874deeae3f
liuhongliang <liuhongliang@kamopos.com>
2018-04-26 17:57:15 +0800

    新增公共帮助类

---
+++ src/Libraries/Kamo.Common/Tools/OrderCodeHelper.cs
@@ -0,0 +1,18 @@
+﻿using Kamo.Common.Tools;
+using System;
+
+namespace Kamo.Common.Tools
+{
+    public static class OrderCodeHelper
+    {
+        public static string GenerateTaskCode(string prefix="",int verificationCodeLength = 3)
+        {
+            var oderCode = string.Empty;
+            var date = DateTime.Now.ToString("yyyyMMdd");
+            DateTime dtFrom = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0, 0);
+            var timeStamp = (Math.Ceiling((DateTime.Now - dtFrom).TotalSeconds)).ToString().PadLeft(5, '0');
+            var randomStr = RandomHelper.RandomNumStr(verificationCodeLength);
+            return prefix + date + timeStamp + randomStr;
+        }
+    }
+}
